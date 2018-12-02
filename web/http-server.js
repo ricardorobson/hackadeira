31975f17e3324934f0c3d44e0b1da5214b69f4a7
Ricardo Robson <rcrdrobson@gmail.com>
2018-12-02 12:45:03 -0300

    Create http server

--- web/http-server.js
+++ web/http-server.js
@@ -1,10 +1,31 @@
-var http = require('http'),
-    fs = require('fs');
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+var path = require('path')
 
+var contentTypes = {
+  'html' : 'text/html',
+  'css'  : 'text/css',
+  'jpg'  : 'image/jpg',
+  'js'   : 'application/javascript'
+}
 
-
-var server = http.createServer(function (request, response) {
-    console.log(request.url);
-    response.write(fs.readFile('./'+request.url);
-    response.end();
+http.createServer(function (req, res) {
+  var urlPath = url.parse(req.url).pathname;
+  
+  if (urlPath==='/') {
+    var filePath = path.join(__dirname, '', urlPath, 'index.html');
+  } else {
+    var filePath = path.join(__dirname, '', urlPath);
+  }
+  fs.readFile(filePath, function (fail, fileData) {
+    if (fail) {
+      res.writeHead(404);
+      res.end();
+    } else {
+      var fileExt = path.extname(filePath).slice(1);
+      res.setHeader('Content-Type', contentTypes[fileExt]);
+      res.end(fileData);
+    }
+  });
 }).listen(8686);
