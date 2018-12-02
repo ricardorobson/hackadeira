48b036369fd1b96349b39eeb3d3e68daa7a465d4
Marcos Barreto <msb5@cin.ufpe.br>
2018-12-02 10:03:21 -0300

    parametrizando o motor

--- motor.py
+++ motor.py
@@ -1,12 +1,16 @@
+import sys
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11,GPIO.OUT)
-pwm=GPIO.PWM(11,50)
-pwm.start(0)
+action = sys.argv[1]
 
-def SetAngle(angle):
+def init_engine():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(11,GPIO.OUT)
+	pwm=GPIO.PWM(11,50)
+	pwm.start(0)
+
+def move_engine(angle):
 	duty = angle / 18 + 2
 	GPIO.output(11, True)
 	pwm.ChangeDutyCycle(duty)
@@ -14,6 +18,12 @@ def SetAngle(angle):
 	GPIO.output(11, False)
 	pwm.ChangeDutyCycle(0)
 
-SetAngle(180) 
+init_engine()
+
+if action == 1: # open
+	move_engine(140)
+elif action == 0: # close
+	move_engine(0)
+
 pwm.stop()
 GPIO.cleanup()
